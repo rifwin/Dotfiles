868acb71d4ab6300d36fb9ee7bffdca0d3f812df
rifwin <itsrifwin@gmail.com>
2025-03-18 04:56:29 +0700

    improve touchpad toggle & init startup

--- hypr/.config/hypr/scripts/touchpad.sh
+++ hypr/.config/hypr/scripts/touchpad.sh
@@ -1,19 +1,66 @@
 #!/bin/bash
 
-arg=$@
-deviceId="${arg#--id=}"
+# args 
+actionName=""
+deviceId=""
 
-#echo $deviceId
-#echo $pwd
-
+# cache file location
 cacheFile="$HOME/.config/hypr/scripts/cache/touchpad"
 
-if [ -f $cacheFile ]; then 
-  notify-send "touchpad off"
-  rm $cacheFile
-  hyprctl keywords device[$deviceId]:enabled false
-else
-  notify-send "touchpad on"
-  touch $cacheFile
-  hyprctl keywords device[$deviceId]:enabled true
-fi
+# if cache file is found then delete file & turn off touchpad
+# if not then create file and turn on touchpad
+touchpadToggle() {
+  if [ -f $cacheFile ]; then 
+    notify-send "touchpad off"
+    rm $cacheFile
+    hyprctl keywords device[$deviceId]:enabled false
+  else
+    notify-send "touchpad on"
+    touch $cacheFile
+    hyprctl keywords device[$deviceId]:enabled true
+  fi
+}
+
+# init touchpad on startup
+# if cache file found then set touchpad on, if not set to off
+touchpadInit() {
+  if [ -f $cacheFile ]; then 
+    hyprctl keywords device[$deviceId]:enabled true
+  else
+    hyprctl keywords device[$deviceId]:enabled false
+  fi
+}
+
+# parse argument
+for arg in "$@"; do
+  case $arg in
+    --id=*)
+      deviceId="${arg#*=}"
+      shift
+      ;;
+    --action=*)
+      actionName="${arg#*=}"
+      shift
+      ;;
+    *)
+      notify-send "Unkown argument: $arg"
+      ;;
+  esac
+done
+
+case $actionName in 
+  toggle)
+    touchpadToggle
+    ;;
+  init)
+    touchpadInit
+    ;;
+  *)
+    notify-send "Unkown action $actionName"
+    ;;
+esac
+
+
+
+
+
