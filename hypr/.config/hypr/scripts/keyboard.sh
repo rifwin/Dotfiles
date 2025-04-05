92e1eb4d8e17601a41149737085d97855128edbd
rifwin <itsrifwin@gmail.com>
2025-04-05 09:05:15 +0700

    keyboard backlit show notification as percent

--- hypr/.config/hypr/scripts/keyboard.sh
+++ hypr/.config/hypr/scripts/keyboard.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-# device id of the keyboard backlight
+# Device ID of the keyboard backlight
 deviceId='asus::kbd_backlight'
 
-currBrightness() {
-  brightnessctl --device=$deviceId get
+percentBright() {
+  currBright=$(brightnessctl --device="$deviceId" get)
+  maxBright=$(brightnessctl --device="$deviceId" max)
+
+  # Use bc for percentage calculation
+  percent=$(echo "scale=2; $currBright / $maxBright * 100" | bc)
+  
+  echo "$percent"
 }
 
-case $@ in
+case $1 in
   lightUp)
-    brightnessctl --device=$deviceId s 10%+
-    notify-send -t 1000 -u low "Keyboard Backlight $(currBrightness)"
+    brightnessctl --device="$deviceId" s 10%+
+    percent=$(percentBright) # Call the function and capture its output
+    notify-send -t 1000 -u low "Keyboard Backlight: $percent%"
     ;;
   lightDown)
-    brightnessctl --device=$deviceId s 10%-
-    notify-send -t 1000 -u low "Keyboard Backlight $(currBrightness)"
+    brightnessctl --device="$deviceId" s 10%-
+    percent=$(percentBright) # Call the function and capture its output
+    notify-send -t 1000 -u low "Keyboard Backlight: $percent%"
     ;;
   *)
-    notify-send -t 1000 -u low "unknown args $@"
+    notify-send -t 1000 -u low "Unknown argument: $1"
     ;;
 esac
-
